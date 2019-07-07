79281f65761c92429051a4ff00d2a4dae234a673
Michael Scarbrough <moonpies27@yahoo.com>
2019-07-07 15:49:24 -0400

    Installed a linter and changed some file configuration
    This represents my first time touching this project since its
    inception. I've installed and configured a linter. I also changed
    some of the project structure to better match what I'm used to at
    work.

---
+++ src/views/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-toolbar dark color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
