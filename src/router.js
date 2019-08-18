c650934426c285a470ef47f2348949ea6dd48858
Michael Scarbrough <michael.scarbrough@nutanix.com>
2019-08-17 21:18:36 -0400

    Started Combat Track component and rearranged some things.

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Landing from './views/Landing.vue'
 
 Vue.use(Router)
 
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Landing
-    },
-    {
-      path: '/home',
       name: 'home',
       component: Home
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-toolbar dark color="purple">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title>
+        D&D 5e Combat Tracker
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <main class="main">
+      <CombatTrack></CombatTrack>
+    </main>
+    <v-footer class="pa-3" absolute="true">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
+import CombatTrack from '@/components/CombatTrack.vue'
 export default {
   components: {
-    HelloWorld
+    CombatTrack
   }
 }
 </script>
+
+<style>
+main {
+  text-align: center;
+  display: block;
+}
+</style>
