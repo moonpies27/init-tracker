a6a20f6458c9474a5bea401a23fe532556b5ba67
Mike <moonpies27@yahoo.com>
2020-02-18 00:01:15 -0500

    Removed some whitespace and reformatted a few lines.

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,27 +1,26 @@
 <template>
   <v-container>
     <v-navigation-drawer
-        v-model="drawer"
-        app
-        clipped
-      >
-        <v-list dense>
-          <v-list-item
-            link
-            v-for="(menuOpt, index) in menuOptions"
-            :key="index"
-            @click="setNavigatorChosenId(menuOpt.id)"
-          >
-            <v-list-item-action>
-              <v-icon>{{ menuOpt.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>{{ menuOpt.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          link
+          v-for="(menuOpt, index) in menuOptions"
+          :key="index"
+          @click="setNavigatorChosenId(menuOpt.id)"
+        >
+          <v-list-item-action>
+            <v-icon>{{ menuOpt.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ menuOpt.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
       <v-app-bar
         app
         clipped-left
