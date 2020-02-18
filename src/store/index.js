87527c82a7a411980286ddb62c3c22f8cb9d77ae
Mike <moonpies27@yahoo.com>
2020-02-18 00:32:58 -0500

    Added vuex-persist module.

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state.js'
 import mutations from './mutations.js'
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
@@ -11,5 +16,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
